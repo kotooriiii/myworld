daed66eb0bdca49a11e97d273c79e4ac51df04c4
kotooriiii <carlosmiranda0643@hotmail.com>
2024-02-14 01:21:51 -0800

    Switching from Ant Design to Mantine.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,53 +1,12 @@
-import {useState} from "react";
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+import { Router } from './Router';
+import { theme } from './theme';
 
-
-import {Button, Layout, theme} from "antd";
-import {MenuFoldOutlined, MenuUnfoldOutlined} from "@ant-design/icons";
-
-import Logo from "./components/Logo.tsx";
-import MenuList from "./components/MenuList.tsx";
-import ToggleThemeButton from "./components/ToggleThemeButton.tsx";
-
-const {Header, Sider} = Layout;
-
-function App()
-{
-    const [darkTheme, setDarkTheme] = useState<boolean>(true);
-    const [collapsed, setCollapsed] = useState<boolean>(false);
-    const toggleTheme: () => void = () =>
-    {
-        setDarkTheme(!darkTheme);
-    };
-
-    const
-        {
-            token: {colorBgContainer},
-        } = theme.useToken();
-
-
-return (
-    <>
-        <Layout>
-            <Sider collapsed={collapsed} collapsible trigger={null} className={'sidebar'} theme={darkTheme ? 'dark' : 'light'}>
-                <Logo/>
-                <MenuList darkTheme={darkTheme}/>
-                <ToggleThemeButton darkTheme={darkTheme} toggleTheme={toggleTheme}/>
-            </Sider>
-            <Layout>
-                <Header style={{padding: 0, background: colorBgContainer}}>
-                    <Button
-                        type='text'
-                        icon={[collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>]}
-                        className='toggle'
-                        onClick={() =>
-                        {
-                            setCollapsed(!collapsed)
-                        }}/>
-                </Header>
-            </Layout>
-        </Layout>
-    </>
-)
+export default function App() {
+  return (
+    <MantineProvider theme={theme}>
+      <Router />
+    </MantineProvider>
+  );
 }
-
-export default App
