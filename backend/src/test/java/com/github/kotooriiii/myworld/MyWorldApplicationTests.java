2f32ae2d572841449c81a3505d19738d1c4ca601
kotooriiii <carlosmiranda0643@hotmail.com>
2024-02-26 01:21:51 -0800

    init backend

---
+++ backend/src/test/java/com/github/kotooriiii/myworld/MyWorldApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github.kotooriiii.myworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWorldApplicationTests
+{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
